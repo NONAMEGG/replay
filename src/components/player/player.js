6f087073ea5391372fe50c915b341f31e43aa31f
NONAMEGG <tyuutyavin@gmail.com>
2024-02-29 15:11:44 +0300

    replay v0.0.4a

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -1,18 +1,23 @@
 import React, { Suspense } from 'react'
+import { useState } from 'react';
+import { tracks } from 'C:/replay/src/data/tracks';
 import './style.css'
 
 const Controls = React.lazy(() => import('./controls'))
 const DisplayTrack = React.lazy(() => import('./displaytrack'))
 const ProgressBar = React.lazy(() => import('./progressbar'))
 
+
 const Player =() => {
+    const [currentTrack, setCurrentTrack] = useState(tracks[0]);
     return (
         <>
             <div className="player__container">
                 
                 <div className="player">
+                <audio src=""controls></audio>
                 <Controls></Controls>    
-                    <DisplayTrack></DisplayTrack> 
+                    <DisplayTrack currentTrack={currentTrack}></DisplayTrack> 
                     <ProgressBar></ProgressBar>
                 </div>
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,4 @@
 import React, { Suspense } from 'react'
-import { Link } from 'react-router-dom'
 
 const Header = React.lazy(() => import('../components/header'))
 
