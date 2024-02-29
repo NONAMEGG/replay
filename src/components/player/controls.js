f43e346e4a440cbdbcd5265500f84e79426cb15a
NONAMEGG <tyuutyavin@gmail.com>
2024-02-29 16:41:53 +0300

    replay v0.1.0a

--- src/components/player/controls.js
+++ src/components/player/controls.js
@@ -1,6 +1,49 @@
 import './style.css'
+import { useState, useEffect } from 'react';
 
-const Controls = () => {
-    return <div>DisplayTrack content here</div>
+
+
+
+const Controls = ({ audioRef }) => {
+  
+  const [isPlaying, setIsPlaying] = useState(false);
+  const togglePlayPause = () => {
+    setIsPlaying((prev) => !prev);
   }
-  export default Controls
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying, audioRef]);
+
+  return (
+    <div className="controls-wrapper">
+      <div className="controls">
+        <button>
+        <img src="/icons/PrevTreck.png" alt="" />
+        </button>
+        <button>
+        <img src="/icons/PrevTreck.png" alt="" />
+        </button>
+
+        <button onClick={togglePlayPause}>
+          {isPlaying ? <img src="/icons/PauseMusic.png" alt="" /> : 
+          <img src="/icons/PlayMusic.png" alt="" />}
+        </button>
+        <button>
+        <img src="/icons/NextTreck.png" alt="" />
+        </button>
+        <button>
+        <img src="/icons/NextTreck.png" alt="" />
+
+        </button>
+      </div>
+    </div>
+  );
+};
+
+
+export default Controls;
--- src/components/player/player.js
+++ src/components/player/player.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react'
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { tracks } from '../data/tracks';
 import './style.css'
 
@@ -9,15 +9,17 @@ const ProgressBar = React.lazy(() => import('./progressbar'))
 
 
 const Player =() => {
+    
+    const audioRef = useRef()
     const [currentTrack, setCurrentTrack] = useState(tracks[0]);
     return (
         <>
             <div className="player__container">
                 
                 <div className="player">
-                <audio src=""controls></audio>
-                <Controls></Controls>    
-                    <DisplayTrack currentTrack={currentTrack}></DisplayTrack> 
+                <Controls audioRef={audioRef}></Controls>    
+                    <DisplayTrack currentTrack={currentTrack}
+                    audioRef={audioRef}></DisplayTrack> 
                     <ProgressBar></ProgressBar>
                 </div>
 
