68d876f59e46fb3d2306cd1c67713b7189b605fd
TipCreg <sivopupkin@gmail.com>
2024-02-29 20:57:23 +0300

    bug

--- src/components/Popup/popup.js
+++ src/components/Popup/popup.js
@@ -5,13 +5,18 @@ import './popup.css'
 
 const popup =() => {
     const ShowPopup = () =>{
-        document.getElementById("p2").style.color="blue";
+        React.Children.toArray(this.props.children).filter((item) => item.props.className === 'Popup_background').style.display = "block";
+        React.Children.toArray(this.props.children).filter((item) => item.props.className === 'main_Popup').style.display = "block";
+    }
+    const HidePopup = () => {
+        React.Children.toArray(this.props.children).filter((item) => item.props.className === 'Popup_background').style.display = "none";
+        React.Children.toArray(this.props.children).filter((item) => item.props.className === 'main_Popup').style.display = "none";
     }
     return (
         <>
         <Suspense fallback={<div>Loading/////</div>}>
         </Suspense>
-        <div className='Popup_background'></div>
+        <div className='Popup_background' onClick={HidePopup}></div>
         <div className='main_Popup'>
                
         </div>
