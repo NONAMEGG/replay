f271169cfe2d3b18a8711a242629819929064e2b
NONAMEGG <tyuutyavin@gmail.com>
2024-02-29 11:35:18 +0300

    replay v0.0.2a

--- src/App.js
+++ src/App.js
@@ -2,31 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <header className='top__header'>
-        хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй  
-      </header>
-      <header className='fixed__header'>
-      <div>ghost::logo</div>
-        <ul className='header__menu'>
-          <li><a href="#"> menuitems</a></li>
-          <li><a href="#"> menuitems</a></li>
-          <li><a href="#"> menuitems</a></li>
-          <li><a href="#"> menuitems</a></li>
-        </ul>
-        <div>profile</div>
-      </header>
-      кркркркркря
-      <div className='main'></div>
-      <div className="player__container">
-        <div className="player">
-          
-        </div>
+return (
+<>
+  <header className='top__header'>
+    хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй
+    хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй хуй
+  </header>
+  <header className='fixed__header'>
+    <div>ghost::logo</div>
+    <ul className='header__menu'>
+      <li><a href="#"> menuitems</a></li>
+      <li><a href="#"> menuitems</a></li>
+      <li><a href="#"> menuitems</a></li>
+      <li><a href="#"> menuitems</a></li>
+    </ul>
+    <div>profile</div>
+  </header>
+  <div className='main'></div>
+  <div className="player__container">
+    <div className="player">
+      
+    </div>
 
-      </div>
-    </>
-  );
+  </div>
+</>
+);
 }
 
 export default App;
