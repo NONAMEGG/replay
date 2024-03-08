3be5d9b0f6702652b5271dfd6b69a36ffab564d4
NONAMEGG <tyuutyavin@gmail.com>
2024-03-09 01:14:30 +0300

    replay v0.2.2a

--- src/components/main/main_section.js
+++ src/components/main/main_section.js
@@ -1,5 +1,5 @@
 import './main_section.css'
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from 'react'
 import { tracks } from '../data/tracks'
 
 
@@ -9,13 +9,30 @@ const Main = ({ trackIndex, setTrackIndex, currentTrack, setCurrentTrack }) => {
         setTrackIndex(index)
         setCurrentTrack(tracks[index])
     }
-        var output = ``
-        tracks.forEach(element => {
-            output += `<div class='track_wrapper'><img src="${element.thumbnail}"
-            class = 'content'></img> <div class='meta_wrapper'><h3>${element.title}</h3><h5>${element.author}</h5></div></div>`
+        // var output = ``
+        // tracks.forEach(element => {
+        //     output += `<div class='track_wrapper'><img src="${element.thumbnail}"
+        //     class = 'content'></img> <div class='meta_wrapper'><h3>${element.title}</h3><h5>${element.author}</h5></div></div>`
+            
+        // })
+
+        const tracks_content = tracks.map((element) => {
+            return (<div onClick={() => changeTrack(tracks.indexOf(element))} className='track_wrapper'><img src={tracks[tracks.indexOf(element)].thumbnail}
+            className = 'content'></img> <div className='meta_wrapper'>
+            <h3>{tracks[tracks.indexOf(element)].title}</h3><h5>{tracks[tracks.indexOf(element)].author}</h5></div></div>)
             
         })
+        console.log(tracks_content)
 
+        
+
+        const renderContent =() => {
+            return (
+                <>
+                    
+                </>
+            )
+        }
         
     
     return (
@@ -24,30 +41,20 @@ const Main = ({ trackIndex, setTrackIndex, currentTrack, setCurrentTrack }) => {
             <div className="wrapper newTracks">
                 <h2 className="new_tracks_header">New Tracks</h2>
                 <div className="container" >
-                <div onClick={() => changeTrack(0)} className='track_wrapper'><img src={tracks[0].thumbnail}
-            className = 'content'></img> <div className='meta_wrapper'><h3>{tracks[0].title}</h3><h5>{tracks[0].author}</h5></div></div>
-                 <div onClick={() => changeTrack(1)} className='track_wrapper'><img src={tracks[1].thumbnail}
-            className = 'content'></img> <div className='meta_wrapper'><h3>{tracks[1].title}</h3><h5>{tracks[1].author}</h5></div></div>
-                
+                {tracks_content}
                 </div>
                 
             </div>
             <div className="wrapper newTracks">
                 <h2 className="new_tracks_header">New Tracks</h2>
                 <div className="container" >
-                <div onClick={() => changeTrack(0)} className='track_wrapper'><img src={tracks[0].thumbnail}
-            className = 'content'></img> <div className='meta_wrapper'><h3>{tracks[0].title}</h3><h5>{tracks[0].author}</h5></div></div>
-                 <div onClick={() => changeTrack(1)} className='track_wrapper'><img src={tracks[1].thumbnail}
-            className = 'content'></img> <div className='meta_wrapper'><h3>{tracks[1].title}</h3><h5>{tracks[1].author}</h5></div></div>
+                    {tracks_content}
                 </div>
             </div>
             <div className="wrapper newTracks">
                 <h2 className="new_tracks_header">New Tracks</h2>
                 <div className="container" >
-                <div onClick={() => changeTrack(0)} className='track_wrapper'><img src={tracks[0].thumbnail}
-            className = 'content'></img> <div className='meta_wrapper'><h3>{tracks[0].title}</h3><h5>{tracks[0].author}</h5></div></div>
-                 <div onClick={() => changeTrack(1)} className='track_wrapper'><img src={tracks[1].thumbnail}
-            className = 'content'></img> <div className='meta_wrapper'><h3>{tracks[1].title}</h3><h5>{tracks[1].author}</h5></div></div>
+                    {tracks_content}
                 </div>
             </div>
         </div>
